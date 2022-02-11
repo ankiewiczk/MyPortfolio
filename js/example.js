d278907aea2d19f90b7e12f653785585cf496fca
Kevin Ankiewicz <ankiewickz@lynnschools.org>
2022-02-11 11:45:46 -0500

    first commit for about me section of personal portfolio

---
+++ js/example.js
@@ -0,0 +1,9 @@
+//Best practice is to lazy load execution after window is loaded
+window.addEventListener("load",loadDocument);
+
+//Define onload event listener Here
+function loadDocument(){
+//Add code here of things you want to be loaded externally
+//Usually used for AJAX requests that populate the webpage.
+
+}
